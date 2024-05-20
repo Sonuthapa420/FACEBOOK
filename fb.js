f1688234eada7945e061e292db337783470f4296
Sonuthapa420 <sonuthapa172004@gmail.com>
2024-05-20 21:22:49 +0545

    may 20 day 2 (too much gap)

---
+++ fb.js
@@ -0,0 +1,15 @@
+const dropdown=document.querySelectorAll(".dd")
+
+dropdown.forEach(dropdown => {
+    const select =dropdown.querySelector(".select")
+    const caret =dropdown.querySelector(".caret")
+    const menu =dropdown.querySelector(".menu")
+
+    select.addEventListener("click", ()=> {
+caret.classList.toggle("caret-rotate")
+menu.classList.toggle("menu-open")
+
+    })
+
+
+})
