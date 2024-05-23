65a0848281db5cecb29c9848330899c159c0f1cd
Sonuthapa420 <sonuthapa172004@gmail.com>
2024-05-23 21:49:44 +0545

    day 4 may 23

--- fb.js
+++ fb.js
@@ -4,6 +4,7 @@ dropdown.forEach(dropdown => {
     const select =dropdown.querySelector(".select")
     const caret =dropdown.querySelector(".caret")
     const menu =dropdown.querySelector(".menu")
+    
 
     select.addEventListener("click", ()=> {
 caret.classList.toggle("caret-rotate")
